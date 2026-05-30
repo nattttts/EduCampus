69d8b9ca93e7bac8e79744add7705a95518741d5
yyyu258 <P23015269@student.newinti.edu.my>
2026-05-30 23:05:18 +0800

    Create AdminDashboard.aspx.cs

---
+++ EduCampus/AdminDashboard.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EduCampus
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Role"] == null)
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
+            if (Session["Role"].ToString() != "Admin")
+            {
+                Response.Redirect("AccessDenied.aspx");
+                return;
+            }
+        }
+    }
+}

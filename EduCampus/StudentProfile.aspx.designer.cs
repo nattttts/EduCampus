29cbfab8a5f3f3c25f6054399facf3de19eb82f4
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-07 18:59:24 +0800

    Create StudentProfile.aspx.designer.cs
    student profile designer

---
+++ EduCampus/StudentProfile.aspx.designer.cs
@@ -0,0 +1,30 @@
+﻿namespace EduCampus
+{
+    public partial class StudentProfile
+    {
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// txtStudentID control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.TextBox txtStudentID;
+
+        /// <summary>
+        /// txtName control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+
+        /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// txtProgramme control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.TextBox txtProgramme;
+    }
+}

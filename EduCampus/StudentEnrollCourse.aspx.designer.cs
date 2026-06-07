5157d8d33f10c64e4ba5db201a4f4fa4a40ff908
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-07 18:59:12 +0800

    Create StudentEnrollCourse.aspx.designer.cs
    Student Enroll course designer

---
+++ EduCampus/StudentEnrollCourse.aspx.designer.cs
@@ -0,0 +1,25 @@
+﻿namespace EduCampus
+{
+    public partial class StudentEnrollCourse
+    {
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// gvCourses control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.GridView gvCourses;
+
+        /// <summary>
+        /// gvEnrollment control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.GridView gvEnrollment;
+
+        /// <summary>
+        /// lblMessage control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Label lblMessage;
+    }
+}

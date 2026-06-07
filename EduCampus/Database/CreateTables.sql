bcbf3fac67f4f3d437c21cad855d3dceefec2bc8
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-07 23:19:53 +0800

    Update CreateTables.sql

--- EduCampus/Database/CreateTables.sql
+++ EduCampus/Database/CreateTables.sql
@@ -80,38 +80,57 @@ CREATE TABLE CourseOfferings (
 
 -- ANNOUNCEMENTS TABLE
 CREATE TABLE Announcements (
-    AnnouncementID INT IDENTITY(1,1) PRIMARY KEY,
-    Title NVARCHAR(100) NOT NULL,
-    Message NVARCHAR(MAX) NOT NULL,
-    PostedDateTime DATETIME DEFAULT GETDATE(),
-    OfferingID INT,
-
     FOREIGN KEY (OfferingID) REFERENCES CourseOfferings(OfferingID)
 );
 
--- ENROLMENTS TABLE
-CREATE TABLE Enrolments (
+-- ENROLLMENTMASTER TABLE
+CREATE TABLE EnrollmentMaster (
     EnrolmentID INT IDENTITY(1,1) PRIMARY KEY,
     DateEnrolled DATE DEFAULT GETDATE(),
     Status VARCHAR(10) DEFAULT 'Pending',
+    Session VARCHAR(10) NOT NULL,
+	Semester VARCHAR(15) NOT NULL,
     StudentID VARCHAR(10) NOT NULL,
+
+    FOREIGN KEY (StudentID) REFERENCES Students(StudentID)
+ );
+
+-- ENROLLMENTDETAILS TABLE
+CREATE TABLE EnrollmentDetails (
+    DetailID INT IDENTITY(1,1) PRIMARY KEY, 
+    EnrolmentID INT NOT NULL,
     OfferingID INT NOT NULL,
 
-    FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+    FOREIGN KEY (EnrolmentID) REFERENCES EnrollmentMaster(EnrolmentID),
     FOREIGN KEY (OfferingID) REFERENCES CourseOfferings(OfferingID)
 );
 
--- ATTENDANCE TABLE
+
 CREATE TABLE Attendance (
-    AttendanceID INT IDENTITY(1,1) PRIMARY KEY,
-    AttendanceDate DATE DEFAULT GETDATE(),
-    Status VARCHAR(10) NOT NULL,
-    Remarks NVARCHAR(255),
-    StudentID VARCHAR(10) NOT NULL,
+    FOREIGN KEY (OfferingID) REFERENCES CourseOfferings(OfferingID)
+);
+
+-- NOTES TABLE
+CREATE TABLE Notes (
+    NoteID INT IDENTITY(1,1) PRIMARY KEY,
+    FileName VARCHAR(255) NOT NULL,
+    FilePath VARCHAR(500) NOT NULL,
+    UploadDate DATETIME DEFAULT GETDATE(),
     OfferingID INT NOT NULL,
 
-    FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
     FOREIGN KEY (OfferingID) REFERENCES CourseOfferings(OfferingID)
+);
+
+-- COURSEMARKS TABLE
+CREATE TABLE CourseMarks (
+    MarkID INT IDENTITY(1,1) PRIMARY KEY,
+    AssignmentMark DECIMAL(5,2),
+    TestMark DECIMAL(5,2),
+    FinalMark DECIMAL (5,2),
+    FinalGrade NVARCHAR(5),
+    DetailID INT NOT NULL,
+
+    FOREIGN KEY (DetailID) REFERENCES EnrollmentDetails(DetailID)
 );
 
 -- RESULTS TABLE
@@ -119,11 +138,14 @@ CREATE TABLE Results (
     ResultID INT IDENTITY(1,1) PRIMARY KEY,
     Mark DECIMAL(5,1),
     Grade NVARCHAR(5),
+    GPA DECIMAL(3,2),
+    CGPA DECIMAL(3,2),
     EnrolmentID INT NOT NULL,
     LecturerID INT NOT NULL,
     StudentID VARCHAR(10) NOT NULL,
 
-    FOREIGN KEY (EnrolmentID) REFERENCES Enrolments(EnrolmentID),
+    FOREIGN KEY (EnrolmentID) REFERENCES EnrollmentMaster(EnrolmentID),
     FOREIGN KEY (LecturerID) REFERENCES Lecturers(LecturerID),
-    FOREIGN KEY (StudentID) REFERENCES Students(StudentID)
+    FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+    FOREIGN KEY (EnrolmentID) REFERENCES EnrollmentMaster(EnrolmentID)
 );

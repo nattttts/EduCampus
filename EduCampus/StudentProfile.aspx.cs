6b1cb8fa78bc7f2f4366a6584132d739c1f6b623
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-08 19:02:13 +0800

    Update StudentProfile.aspx.cs

--- EduCampus/StudentProfile.aspx.cs
+++ EduCampus/StudentProfile.aspx.cs
@@ -65,6 +65,11 @@ namespace EduCampus
                     txtProgramme.Text = reader["ProgrammeName"].ToString();
                 }
             }
+
+        }
+        protected void btnBackDashboard_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("StudentDashboard.aspx");
         }
     }
 }

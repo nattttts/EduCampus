bea02b1fb035b7e6d2f1e6fa61fe19e83f134f0b
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-08 21:12:29 +0800

    Update Results.aspx.cs

--- EduCampus/Results.aspx.cs
+++ EduCampus/Results.aspx.cs
@@ -66,9 +66,6 @@ namespace EduCampus
                 gvResults.DataBind();
             }
         }
-        protected void btnBackDashboard_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("StudentDashboard.aspx");
-        }
+
     }
 }

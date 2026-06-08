ac106f5a2aa6620a5240a1b27cf78069ba6e2fd1
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-08 20:43:37 +0800

    Create Results.aspx.designer.cs

---
+++ EduCampus/Results.aspx.designer.cs
@@ -0,0 +1,33 @@
+﻿namespace EduCampus
+{
+
+
+    public partial class Results
+    {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// btnBackDashboard control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Button btnBackDashboard;
+
+        /// <summary>
+        /// btnSem1 control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Button btnSem1;
+
+        /// <summary>
+        /// btnSem2 control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Button btnSem2;
+
+        /// <summary>
+        /// gvResults control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.GridView gvResults;
+    }
+}

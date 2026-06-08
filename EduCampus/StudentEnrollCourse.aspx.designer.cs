7254780cc8d72b8e9799aace403ec0803c371f2e
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-08 21:12:42 +0800

    Update StudentEnrollCourse.aspx.designer.cs

--- EduCampus/StudentEnrollCourse.aspx.designer.cs
+++ EduCampus/StudentEnrollCourse.aspx.designer.cs
@@ -2,24 +2,12 @@
 {
     public partial class StudentEnrollCourse
     {
-        /// <summary>
-        /// form1 control.
-        /// </summary>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
-        /// <summary>
-        /// gvCourses control.
-        /// </summary>
         protected global::System.Web.UI.WebControls.GridView gvCourses;
 
-        /// <summary>
-        /// gvEnrollment control.
-        /// </summary>
         protected global::System.Web.UI.WebControls.GridView gvEnrollment;
 
-        /// <summary>
-        /// lblMessage control.
-        /// </summary>
         protected global::System.Web.UI.WebControls.Label lblMessage;
     }
 }

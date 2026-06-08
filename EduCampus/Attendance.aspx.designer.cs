15c167e638db1e8107820a46810092f390a4f037
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-08 20:43:34 +0800

    Create Attendance.aspx.designer.cs

---
+++ EduCampus/Attendance.aspx.designer.cs
@@ -0,0 +1,28 @@
+﻿namespace EduCampus
+{
+
+
+    public partial class Attendance
+    {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// btnBackDashboard control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Button btnBackDashboard;
+
+        /// <summary>
+        /// ddlCourse control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCourse;
+
+        /// <summary>
+        /// gvAttendance control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.GridView gvAttendance;
+    }
+}

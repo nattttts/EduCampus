54b44d02c60a49ad45ee267dd3f3fb6dadf09ffe
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-13 01:58:05 +0800

    Create StudentNotes.aspx.designer.cs

---
+++ EduCampus/StudentNotes.aspx.designer.cs
@@ -0,0 +1,22 @@
+﻿namespace EduCampus
+{
+    public partial class StudentNotes
+    {
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// lblCourseName control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Label lblCourseName;
+
+        /// <summary>
+        /// gvNotes control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.GridView gvNotes;
+
+        /// <summary>
+        /// btnLogout control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Button btnLogout;
+    }
+}

83cd24042dbe4f213dff01b59f7c8cdb1d00dc8f
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-13 02:44:38 +0800

    Create StudentNotification.aspx.designer.cs

---
+++ EduCampus/StudentNotification.aspx.designer.cs
@@ -0,0 +1,20 @@
+﻿namespace EduCampus
+{
+    public partial class StudentNotification
+    {
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// gvNotifications control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.GridView gvNotifications;
+
+        /// <summary>
+        /// btnLogout control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Button btnLogout;
+    }
+}

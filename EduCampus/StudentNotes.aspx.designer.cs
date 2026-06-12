3643993de135540e2687b5659280cab984ec179f
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-13 02:44:29 +0800

    Update StudentNotes.aspx.designer.cs

--- EduCampus/StudentNotes.aspx.designer.cs
+++ EduCampus/StudentNotes.aspx.designer.cs
@@ -2,7 +2,15 @@
 {
     public partial class StudentNotes
     {
+        /// <summary>
+        /// form1 control.
+        /// </summary>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// lblMessage control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.Label lblMessage;
 
         /// <summary>
         /// lblCourseName control.

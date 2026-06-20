aad992ed1cf6af00939955c2953346f448c6b79e
yyyu258 <P23015269@student.newinti.edu.my>
2026-06-20 16:44:48 +0800

    Update Results.aspx.designer.cs

--- EduCampus/Results.aspx.designer.cs
+++ EduCampus/Results.aspx.designer.cs
@@ -1,9 +1,7 @@
 ﻿namespace EduCampus
 {
-
     public partial class Results
     {
-
         /// <summary>
         /// form1 control.
         /// </summary>
@@ -18,6 +16,26 @@
         /// btnSem2 control.
         /// </summary>
         protected global::System.Web.UI.WebControls.Button btnSem2;
+
+        // ===== STUDENT INFO LABELS =====
+
+        protected global::System.Web.UI.WebControls.Label lblStudentName;
+
+        protected global::System.Web.UI.WebControls.Label lblStudentID;
+
+        protected global::System.Web.UI.WebControls.Label lblProgramme;
+
+        protected global::System.Web.UI.WebControls.Label lblSession;
+
+        protected global::System.Web.UI.WebControls.Label lblSemester;
+
+        protected global::System.Web.UI.WebControls.Label lblDate;
+
+        // ===== RESULT LABELS =====
+
+        protected global::System.Web.UI.WebControls.Label lblGPA;
+
+        protected global::System.Web.UI.WebControls.Label lblCGPA;
 
         /// <summary>
         /// gvResults control.

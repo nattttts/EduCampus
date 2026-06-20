f1d0a787debc9a1e0479e82c8edf5b8f12f159fe
nattttts <natalieshee@gmail.com>
2026-06-20 10:48:25 +0800

    Add sample data for Notes table

--- EduCampus/Database/InsertSampleData.sql
+++ EduCampus/Database/InsertSampleData.sql
@@ -129,3 +129,27 @@ VALUES
 ('Apr2026', '2026-05-01', '2026-05-01', 'Public Holiday: Labour Day'),
 
 ('Apr2026', '2026-05-18', '2026-05-24', 'Mid Semester Break');
+
+INSERT INTO Notes
+(FileName, FilePath, OfferingID)
+VALUES
+
+-- DATA STRUCTURES NOTES
+('Week1_OOD_Design.pdf',
+ '~/Uploads/DataStructures/Week1_OOD_Design.pdf',
+2),
+
+('Week2_OOD_Design_Part2.pdf',
+ '~/Uploads/DataStructures/Week2_OOD_Design_Part2.pdf',
+2),
+
+-- INTRO TO STATS AND ANALYTICS NOTES
+('DCS1104_Chapter1.pdf',
+ '~/Uploads/IntroToStatisticsAndAnalytics/DCS1104_Chapter1.pdf',
+1),
+
+('DCS1104_Chapter2.pdf',
+ '~/Uploads/IntroToStatisticsAndAnalytics/DCS1104_Chapter2.pdf',
+1);
+
+SELECT * FROM Notes;

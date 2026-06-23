e993ff7de1e054135efd4f8da0d47537a63e2199
nattttts <natalieshee@gmail.com>
2026-06-23 17:34:18 +0800

    Update CreateTables.sql

--- EduCampus/Database/CreateTables.sql
+++ EduCampus/Database/CreateTables.sql
@@ -82,6 +82,12 @@ CREATE TABLE CourseOfferings (
 
 -- ANNOUNCEMENTS TABLE
 CREATE TABLE Announcements (
+    AnnouncementID INT IDENTITY(1,1) PRIMARY KEY,
+    Title NVARCHAR(100) NOT NULL,
+    Message NVARCHAR(MAX) NOT NULL,
+    PostedDateTime DATETIME DEFAULT GETDATE(),
+    OfferingID INT,
+
     FOREIGN KEY (OfferingID) REFERENCES CourseOfferings(OfferingID)
 );
 
